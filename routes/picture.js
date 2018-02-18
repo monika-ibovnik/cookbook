df719391dc68f3c627154194f61064eaa46ba3be
monika <ibovnik.mo@gmail.com>
2018-02-18 13:24:45 +0100

    saved files have random uidSafe names, next step is uploading to s3

--- routes/picture.js
+++ routes/picture.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-const path = require('path');
+const uidSafe = require('uid-safe');
 const Query = require('../models/query.js');
 
 router.post('/picture/upload', (req,res)=>{
     let {imgBase64} = req.body;
-    let imgToSave = imgBase64.replace(/^data:image\/png;base64,/, "");
-    fs.writeFile("img.png", imgToSave, 'base64', (err)=>{
-        console.log('error', err);
+    let imgToSave = imgBase64.replace(/^data:image\/png;base64,/, '');
+    uidSafe(24).then(uid=>{
+        fs.writeFile(`./uploads/${uid}.png`, imgToSave, 'base64', (err)=>{
+            console.log('error', err);
+        });
     });
 
 });
